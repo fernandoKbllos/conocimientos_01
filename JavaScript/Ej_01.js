5e0da0cf85ab777c4764b3dc5d8c9ceebb41cf91
fernandoKbllos <fkabellos@gmail.com>
2022-02-01 21:36:52 +0100

    Update Ej_01.js

--- JavaScript/Ej_01.js
+++ JavaScript/Ej_01.js
@@ -2,6 +2,30 @@
 
 console.log("Hola ejercicio 1")
 
+const num = 100
+let contrasena = Math.round(Math.random(0,100) *num);
+console.log(contrasena)
 
-const contraseña = Math.random(0,100);
-console.log(contraseña)
+
+for (let i=0; i<=4; i++){
+
+const num_usuario = prompt("Dime un numero del 1 al 100")
+
+    if (parseInt(num_usuario) === contrasena) {
+     alert("Has ganado");
+     break;
+    }
+    else if ( i === 4) {
+        alert("Has perdido");
+        break;
+    }    
+    
+    else if (parseInt(num_usuario) < contrasena) {
+        alert("El numero que buscas es mayor")
+    }
+    else if (parseInt(num_usuario) > contrasena) {
+        alert("El numero que buscas es menor")
+    }
+    
+}
+
